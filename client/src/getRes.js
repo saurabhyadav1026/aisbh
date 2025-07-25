0e6f0fc840011095437c35d3d549a7c33bb80138
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2025-07-25 11:19:42 +0530

    sss

--- client/src/getRes.js
+++ client/src/getRes.js
@@ -2,7 +2,7 @@
 
 
 const getRes=(req)=>{
-let rrr= fetch(process.env.REACT_APP_API_KEY+'/api/sbh?reqq='+req)
+let rrr= fetch(process.env.REACT_APP_API_KEY+'/api/sbh?reqq='+req||"")
 .then(response => response.json())
 .then(data => {
     return data.sbh;
